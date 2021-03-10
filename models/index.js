e510cf2c995bad1225e1eb4c13e97c1f86790bad
Vicky Loebel <vickyloebel@gmail.com>
2021-03-10 12:12:56 -0700

    added associations to models.index.js

--- models/index.js
+++ models/index.js
@@ -3,14 +3,45 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
-
-// Products belongsTo Category
+const { belongsTo } = require('./Product');
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+//vlnote: confused about which foreignKey to use!
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'inventory',
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'inventory',
+    foreignKey: 'tag_id'
+});
+// following the code in the module but don't
+// really see why this has to be double defined:
+
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id'
+});
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
+Product.hasMany(ProductTag, {
+  foreignKey: 'product_id'
+})
+Tag.hasMany(ProductTag, {
+  foreignKey: 'tag_id'
+});
+
 
 module.exports = {
   Product,
